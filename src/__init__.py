630badbfc17eab4beb86329350ee606a6c03feb5
faduzin <eric.fadul@gmail.com>
2025-02-17 23:49:21 -0300

    module header for import *

---
+++ src/__init__.py
@@ -0,0 +1,16 @@
+from src.preprocessing import class_remapping, scale_data, extract_individual_n
+from src.utils import load_data, save_data
+from src.eda import data_info, plot_correlations, count_classes, plot_confusion_matrix, plot_boxplot_and_histogram
+
+__all__ = [
+    'load_data', 
+    'save_data', 
+    'class_remapping', 
+    'scale_data', 
+    'data_info', 
+    'plot_correlations', 
+    'count_classes', 
+    'plot_confusion_matrix', 
+    'plot_boxplot_and_histogram',
+    'extract_individual_n'
+]
