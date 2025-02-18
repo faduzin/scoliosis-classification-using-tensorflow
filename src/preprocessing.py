ee17ab7f88f18dd24579a2096f5f2819896c8221
faduzin <eric.fadul@gmail.com>
2025-02-17 23:48:47 -0300

    preprocessing functions

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,9 +1,34 @@
 import pandas as pd
+from sklearn.preprocessing import StandardScaler
 
 
 def class_remapping(data, column_name):
-    data['class'] = (data['Scolio'] > 10).astype(int)
-    return data
+    try:
+        data['class'] = (data['Scolio'] > 10).astype(int)
+        return data
+    except Exception as e:
+        print(f"Error: {e}")
+        return None
 
-def test():
-    print('Hello world!')
+
+def scale_data(data):
+    try:    
+        scaler = StandardScaler()
+        data = scaler.fit_transform(data)
+        return data
+    except Exception as e:
+        print(f"Error: {e}")
+        return None
+    
+
+def extract_individual_n(data):
+    try:
+        temp_list = []
+        for name in data['Name']:
+            temp_list.append(name.split('_')[1])
+        data['individual_n'] = temp_list
+        print('Individual number extracted successfully.')
+        return data
+    except Exception as e:
+        print(f"Error: {e}")
+        return None
