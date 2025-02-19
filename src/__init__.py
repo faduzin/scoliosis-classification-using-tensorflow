d284a7818fcb09b0374d1dbf405b217a198a04e3
faduzin <eric.fadul@gmail.com>
2025-02-19 13:37:07 -0300

    updating init

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,7 @@
-from src.preprocessing import class_remapping, scale_data, extract_individual_n
+from src.preprocessing import class_remapping, scale_data, extract_individual_n, stratified_group_split
 from src.utils import load_data, save_data
 from src.eda import data_info, plot_correlations, count_classes, plot_confusion_matrix, plot_boxplot_and_histogram
+from src.mlp import build_mlp, build_tuned_mlp
 
 __all__ = [
     'load_data', 
@@ -12,5 +13,8 @@ __all__ = [
     'count_classes', 
     'plot_confusion_matrix', 
     'plot_boxplot_and_histogram',
-    'extract_individual_n'
+    'extract_individual_n',
+    'stratified_group_split',
+    'build_mlp',
+    'build_tuned_mlp'
 ]
